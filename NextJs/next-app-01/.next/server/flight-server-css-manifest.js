7f9d6ce6b095cf3e1bf7cab889be73cfd3a6bb2b
malonicester <ashish.tripathy39@gmail.com>
2023-07-02 01:19:10 +0530

    uncommited changes

---
+++ NextJs/next-app-01/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,22 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\page.tsx": [
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\page.module.css"
+    ],
+    "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\layout.tsx": [
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\page": [
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\page.module.css",
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\globals.css",
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\about\\page": [
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\app\\globals.css",
+      "C:\\Users\\ashis\\.vscode\\Masai\\React\\NextJs\\next-app-01\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
